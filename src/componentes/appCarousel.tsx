e9f6e1dbb24b10e03119ea57e2be8ed0b701539c
Axholutions <axholutions@gmail.com>
2023-01-19 23:49:05 -0500

    Update index.html, appCarousel.tsx, and appCarousel.css

--- src/componentes/appCarousel.tsx
+++ src/componentes/appCarousel.tsx
@@ -1,37 +1,31 @@
-import { useState } from "react"
+import "../style/appCarousel.css"
 
 export const Carousel = () => {
-
-    let imagenes: string[] = [
-        "resources/Dron",
-        "resources/Dron",
-        "resources/Dron"
-    ]
-
-
-
-    const [imagen, setImagen] = useState("resources/Dron");
-
-    //const cambiarImagen = ()
-
     return (
-        <div className='carousel-general' id="carousel-general">
-            <div className='carousel-inner'>
-                {
-                        imagenes.map((imagenes, index) => {
-                            return (
-                                <div className='carousel-item' key={index}>
-                                    <img src={ imagenes + (index + 1) + ".jpg" } className="imagen" alt={"Dron image" + index} />
-                                </div>
-                            )
-                        })
-                }
+        <div id="carouselExampleDark" className="carousel carousel-dark slide">
+            <div className="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
-            <button className='btn-carousel-prev' type='button'>
-                <h1> Anterior </h1>
+            <div className="carousel-inner">
+                <div className="carousel-item active" data-bs-interval="2000">
+                    <img src="resources/Dron1.jpg" className="d-block w-100" alt="Dron Volaré"/>
+                </div>
+                <div className="carousel-item" data-bs-interval="2000">
+                    <img src="resources/Dron2.jpg" className="d-block w-100" alt="Dron de Volaré"/>
+                </div>
+                <div className="carousel-item">
+                    <img src="resources/Dron3.jpg" className="d-block w-100" alt="Dron de Volaré"/>
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
             </button>
-            <button className='btn-carousel-prev' type='button'>
-                <h1> Siguiente </h1>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
             </button>
         </div>
     )
