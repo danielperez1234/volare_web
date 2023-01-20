51bbab25dea3b49de01d7b331f3b1d0a19dde1c7
Axholutions <axholutions@gmail.com>
2023-01-19 23:08:15 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './componentes/appNavbar';
+import { Carousel } from './componentes/appCarousel';
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
       <header className="App-header">
         <Navbar/>
       </header>
+      <body>
+        <Carousel/>
+      </body>
     </div>
   );
 }
