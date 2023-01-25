aaff7a523852de22fea38bed7a4c14d0be7cedc2
dan120929 <dan.salgado58d@gmail.com>
2023-01-24 20:46:48 -0600

    Actualizar appVideo.tsx

--- src/componentes/appVideo.tsx
+++ src/componentes/appVideo.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import "../style/Video.css";
+import React, {  useRef, useState } from 'react';
+import "../style/appVideo.css";
 
 export const Video = () => {
+    const [isPlay, setPlay] = useState(false);
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const handleClick = (e:React.MouseEvent<HTMLVideoElement>)=>{
+        setPlay(!isPlay);
+        if(isPlay){
+            videoRef.current?.play();
+        }else{
+            videoRef.current?.pause();
+        }
+    }
     return (
-        <div className="video">
-            <video autoPlay muted loop id="myVideo">
+        <div className='video-margin'>
+        <div className={"video "+(!isPlay?"":"video-pause")}>
+            <video className={""+(!isPlay?"":"player-pause")} ref={videoRef}  onClick={handleClick}  loop id="myVideo">
                 <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4" />
             </video>
+        </div>
+        
         </div>
     );
 }
