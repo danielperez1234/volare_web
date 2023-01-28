3c239bd6a99eadcf56277f9e3a635e6fd1347157
Axholutions <axholutions@gmail.com>
2023-01-28 00:41:23 -0500

    Ajustes al appSection

--- src/componentes/appSection.tsx
+++ src/componentes/appSection.tsx
@@ -14,6 +14,9 @@ export const Section = () => {
                     <img className="imagen" src="https://th.bing.com/th/id/R.5b5b8a8ca427f42ae6c84d005150985f?rik=S2ugNbl95XHe1Q&riu=http%3a%2f%2f1.bp.blogspot.com%2f_Lf-tsUDf7BY%2fSk6TxZ4j_MI%2fAAAAAAAAAJA%2fSRre6GaprP8%2fs400%2fpene%2bdibujo.jpg&ehk=osvtoxfzYTheptZFd93JXnMrbP9tcqEz9C7a5JguDGw%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1"/>
                     <img className="imagen" src="https://th.bing.com/th/id/R.5b5b8a8ca427f42ae6c84d005150985f?rik=S2ugNbl95XHe1Q&riu=http%3a%2f%2f1.bp.blogspot.com%2f_Lf-tsUDf7BY%2fSk6TxZ4j_MI%2fAAAAAAAAAJA%2fSRre6GaprP8%2fs400%2fpene%2bdibujo.jpg&ehk=osvtoxfzYTheptZFd93JXnMrbP9tcqEz9C7a5JguDGw%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1"/>
                 </div>
+                <div className="trama">
+                    <img className="imagenTrama" src="https://images.freeimages.com/images/large-previews/3b7/halftone-1162234.jpg"/>
+                </div>
             </div>            
         </div>
     )
